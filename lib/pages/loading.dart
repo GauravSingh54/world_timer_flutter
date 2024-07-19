fb0deb7ae7bd5959258970a561ecf9a9b1ed8b05
Gaurav Singh <2022pietcagaurav022@poornima.org>
2024-07-19 16:34:20 +0530

    1 2 ka 4

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
+import 'package:world_timer_flutter/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -13,68 +11,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = 'loading';
 
-  void getTime() async {
-    // final Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    // http.Response response = await http.get(url);
-    // Map data = jsonDecode(response.body);
-    // print(data);
-    // print(data['title']);
-
-    // final Uri url = Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata');
-    // final Uri url = Uri.parse('https://worldtimeapi.org/api/timezone/Europe/Moscow');
-    // final Uri url = Uri.parse('https://worldtimeapi.org/api/timezone/Etc/GMT-7');
-    final Uri url = Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London');
-    http.Response response = await http.get(url);
-    Map data = jsonDecode(response.body);
-    // print(data);
-
-    // get properties from data
-
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'];
-    // print('Datetime : $datetime');
-    // print('Offset : $offset');
-
-
-     if (offset.length >= 3) {
-      // Default to hours only
-      int offsetHours = int.parse(offset.substring(1, 3));
-      int offsetMinutes = 0;
-
-      // If offset has minutes, parse them
-      if (offset.length >= 6) {
-        offsetMinutes = int.parse(offset.substring(4, 6));
-      }
-
-      // Create a DateTime object and add/subtract the offset
-      DateTime now = DateTime.parse(datetime);
-      Duration offsetDuration = Duration(hours: offsetHours, minutes: offsetMinutes);
-      if (offset.startsWith('-')) {
-        now = now.subtract(offsetDuration);
-      } else {
-        now = now.add(offsetDuration);
-      }
-
-      print('Current Time: $now');
-    } else {
-      print('Invalid offset format: $offset');
-    }
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
-
-
 
   @override
   void initState() {
     super.initState();
-      getTime();  
-    }
-
+    setupWorldTime();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text('Loading Screen')),
-    );
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(50),
+      child: Text(time),
+    ));
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: avoid_print
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+class WorldTime {
+
+  late String location;  //location name for the UI
+  late String time; //Time in that location
+  late String flag; // URL to asset flag icon
+  late String url; // location url for api end point
+
+  WorldTime({ required this.location, required this.flag, required this.url});
+
+  Future <void> getTime() async {
+    
+    try{
+
+      
+
+    // final Uri url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    // http.Response response = await http.get(url);
+    // Map data = jsonDecode(response.body);
+    // print(data);
+    // print(data['title']);
+
+    final Uri apiurl = Uri.parse('https://worldtimeapi.org/api/timezone/$url');
+    http.Response response = await http.get(apiurl);
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    // get properties from data
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'];
+    // print('Datetime : $datetime');
+    // print('Offset : $offset');
+
+
+     if (offset.length >= 3) {
+      // Default to hours only
+      int offsetHours = int.parse(offset.substring(1, 3));
+      int offsetMinutes = 0;
+
+      // If offset has minutes, parse them
+      if (offset.length >= 6) {
+        offsetMinutes = int.parse(offset.substring(4, 6));
+      }
+
+      // Create a DateTime object and add/subtract the offset
+      DateTime now = DateTime.parse(datetime);
+      Duration offsetDuration = Duration(hours: offsetHours, minutes: offsetMinutes);
+      if (offset.startsWith('-')) {
+        now = now.subtract(offsetDuration);
+      } else {
+        now = now.add(offsetDuration);
+      }
+
+      time = now.toString();
+
+      print('Current Time: $now');
+    } else {
+      print('Invalid offset format: $offset');
+    }
+      
+    }
+
+    catch (e) {
+      print('Caught Error: $e');
+      time = 'Could not get time data';
+    }
+
+
+  }
+
+
+}
+
+
