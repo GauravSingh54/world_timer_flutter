4dc42c363e90e03fe800a9396a580cc05ee4a8ea
Gaurav Singh <2022pietcagaurav022@poornima.org>
2024-07-21 00:32:45 +0530

    Done

---
+++ lib/pages/search.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:world_timer_flutter/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  List<WorldTime> filteredLocations = [];
+  TextEditingController searchController = TextEditingController();
+  String searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    filteredLocations = locations;
+    searchController.addListener(() {
+      setState(() {
+        searchQuery = searchController.text;
+        filteredLocations = locations.where((location) {
+          return location.location.toLowerCase().contains(searchQuery.toLowerCase());
+        }).toList();
+      });
+    });
+  }
+
+  void updateTime(index) async {
+    WorldTime instance = filteredLocations[index];
+    await instance.getTime();
+    // navigate to home screen
+
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build State Function ran');
+
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Choose a Location', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                labelText: 'Search',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredLocations.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+                  child: Card(
+                    child: ListTile(
+                      onTap: () {
+                        updateTime(index);
+                      },
+                      title: Text(filteredLocations[index].location),
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage('assets/${filteredLocations[index].flag}'),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
